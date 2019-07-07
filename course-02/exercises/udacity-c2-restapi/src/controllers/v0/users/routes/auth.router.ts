93276766663623b30c2d23a716b077242971b19f
Scott Litz <scott.litz@gmail.com>
2019-07-07 04:29:48 -0700

    commented out require auth code

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -22,28 +22,29 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 }
 
 function generateJWT(user: User): string {
-    return jwt.sign(JSON.parse(JSON.stringify(user));, config.dev.secret);
+    return jwt.sign(JSON.parse(JSON.stringify(user)), config.dev.secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    if (!req.headers || !req.headers.authorization){
-        return res.status(401).send({ message: 'No authorization headers.' });
-    }
-
-    // Bearer asdlfkasjdlfkasdf...
-    const token_bearer = req.headers.authorization.split(' ');
-    if(token_bearer.length != 2){
-        return res.status(401).send({ message: 'Malformed token.' });
-    }
-
-    const token = token_bearer[1];
-
-    return jwt.verify(token, config.dev.secret, (err, decoded) => {
-      if (err) {
-        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-      }
-      return next();
-    });
+    return next();
+    // if (!req.headers || !req.headers.authorization){
+    //     return res.status(401).send({ message: 'No authorization headers.' });
+    // }
+    //
+    // // Bearer asdlfkasjdlfkasdf...
+    // const token_bearer = req.headers.authorization.split(' ');
+    // if(token_bearer.length != 2){
+    //     return res.status(401).send({ message: 'Malformed token.' });
+    // }
+    //
+    // const token = token_bearer[1];
+    //
+    // return jwt.verify(token, config.dev.secret, (err, decoded) => {
+    //   if (err) {
+    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+    //   }
+    //   return next();
+    // });
 }
 
 router.get('/verification',
